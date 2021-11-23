fc678cf099e16f16f0373008ee9897cddfa971cb
Anand <anandnatarajanbtech@gmail.com>
2021-11-23 15:48:35 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { trimTrailingNulls } from '@angular/compiler/src/render3/view/util';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'demo';
+  categorylist:any;
+}
+
